d292ee9e1204219c0843fce9cb1dcc562512e1c5
kdbtfzcoe <164982209+kdbtfzcoe@users.noreply.github.com>
2024-04-01 22:25:36 +0800

    Add files via upload

---
+++ HelloGoodbye/HelloGoodbye.cs
@@ -0,0 +1,33 @@
+﻿
+namespace HelloGoodbye
+    {
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                for (int num1_100 = 1; num1_100 <= 100; num1_100++)
+                {
+                    if (num1_100 % 3 == 0 & num1_100 % 5 == 0)
+                    {
+                        Console.WriteLine("Hello Goodbye");
+                    }
+                    if (num1_100 % 5 == 0)
+                    {
+                        Console.WriteLine("Goodbye");
+                    }
+                    if (num1_100 % 3 == 0)
+                    {
+                        Console.WriteLine("Hello");
+                    }
+                    else
+                    {
+                        Console.WriteLine(num1_100);
+                    }
+
+
+                }
+            }
+        }
+    }
+
+    
